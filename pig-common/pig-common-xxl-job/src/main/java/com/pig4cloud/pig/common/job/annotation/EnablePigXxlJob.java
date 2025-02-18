34a1e344fb4dad7df182ebec1e13d816ec18d833
TechGrowX <1580035000@qq.com>
2025-02-19 01:15:52 +0800

    feat(xxl-job): 新增 XXL-JOB 分布式定时任务的支持

---
+++ pig-common/pig-common-xxl-job/src/main/java/com/pig4cloud/pig/common/job/annotation/EnablePigXxlJob.java
@@ -0,0 +1,21 @@
+package com.pig4cloud.pig.common.job.annotation;
+
+import com.pig4cloud.pig.common.job.XxlJobAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * 激活xxl-job配置
+ *
+ * @author techgrow
+ * @date 2025/02/18
+ */
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({ XxlJobAutoConfiguration.class })
+public @interface EnablePigXxlJob {
+
+}
